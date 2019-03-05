4b9219f405513311a180a39ed15c2e3237aa99b0
Mostafa Hamdaoui <mostafahamdaoui41@gmail.com>
2019-03-05 08:45:03 +0100

    change espace collaborateur

---
+++ public/assets/js/app.js
@@ -0,0 +1,8 @@
+var qrcode = new QRCode("test", {
+    text: "scanner",
+    width: 128,
+    height: 128,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
